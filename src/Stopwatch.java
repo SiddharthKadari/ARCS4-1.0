bf341854e34044c9d577be4fb2f21acdcde8392e
Siddharth Kadari <kadaris@gmail.com>
2024-07-04 17:33:35 -0700

    improved stopwatch functionality

--- src/Stopwatch.java
+++ src/Stopwatch.java
@@ -1,9 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Stopwatch {
 	//Stopwatch is paused when lastStart = 0
 	private long lastStart = 0, accumTime = 0;
+	private HashMap<String, Double> recordedTimes = new HashMap<>();
 
 	//Resets the elapsed time on the stopwatch and pauses
 	public void reset(){
+		accumTime = 0;
+		lastStart = 0;
+	}
+
+	//Resets the elapsed time on the stopwatch and pauses. Records the time previously tracked in a Hashmap linked to the provided label
+	public void reset(String label){
+		recordedTimes.put(label, nanos());
 		accumTime = 0;
 		lastStart = 0;
 	}
@@ -36,5 +48,29 @@ public class Stopwatch {
 
 	public double nanos(){
 		return lastStart == 0 ? accumTime : accumTime + System.nanoTime() - lastStart;
+	}
+
+	public double secs(String label){
+		return nanos(label) / 1000000000;
+	}
+
+	public double millis(String label){
+		return nanos(label) / 1000000;
+	}
+
+	public double micros(String label){
+		return nanos(label) / 1000;
+	}
+
+	public double nanos(String label){
+		return recordedTimes.get(label);
+	}
+
+	public void printAllRecords(){
+		Set<Map.Entry<String, Double>> record = recordedTimes.entrySet();
+
+		for(Map.Entry<String, Double> entry : record){
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+		}
 	}
 }
