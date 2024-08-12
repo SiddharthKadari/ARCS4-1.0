02dd16e31e3d02731e4fca7ffc59a2db0192ba0a
Siddharth Kadari <kadaris@gmail.com>
2024-08-11 20:18:49 -0700

    Camera Capture and display test complete

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,9 @@ import threephase.FullCube;
 import threephase.Search;
 import threephase.Tools;
 
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.bytedeco.librealsense.frame;
 import org.bytedeco.opencv.opencv_core.*;
 import org.bytedeco.opencv.opencv_videoio.*;
 import static org.bytedeco.opencv.global.opencv_core.*;
@@ -26,6 +29,12 @@ public class Main {
 	private static final boolean TESTING_ARDUINO = false && USING_ARDUINO;
 	private static final boolean TESTING_WEBCAM = true && USING_WEBCAM;
 	private static final boolean TESTING_SOLVER_ARDUINO = false && USING_SOLVER && USING_ARDUINO;
+
+	private static final int CAMERA_WIDTH = 640;
+	private static final int CAMERA_HEIGHT = 480;
+	private static final int DISPLAY_WIDTH = 200;
+	private static final int DISPLAY_HEIGHT = 200;
+	
 
 	static {
 		System.setProperty("org.bytedeco.openblas.load", "mkl");
@@ -79,8 +88,8 @@ public class Main {
 
 			System.out.println("Connecting to webcam...");
 			capture = new VideoCapture(0);
-			capture.set(CAP_PROP_FRAME_WIDTH, 640);
-			capture.set(CAP_PROP_FRAME_HEIGHT, 480);
+			capture.set(CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH);
+			capture.set(CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT);
 
 			if (!capture.isOpened()) {
 				System.out.println("Error opening webcam");
@@ -117,18 +126,59 @@ public class Main {
 		}
 	
 		if(TESTING_WEBCAM){
+			//create datastructures needed for camera test
 			Mat frame = new Mat();
-			if (capture.read(frame)) {
-				byte[] bgr = new byte[frame.channels()];
-				int[] bgrUnsigned = new int[frame.channels()];
+			Mat displayGrid = new Mat(DISPLAY_HEIGHT * 3, DISPLAY_WIDTH * 4, CV_8UC3, new Scalar(255, 255, 255, 255));
+			
+			OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat();
+			// Define the positions for each image
+			int[][] positions = {
+				{1, 0}, // image1
+				{0, 1}, // image2
+				{1, 1}, // image3
+				{2, 1}, // image4
+				{3, 1}, // image5
+				{1, 2}  // image6
+			};
 
-				//get pixels with: frame.ptr(x, y).get(rgb);, rgb will be stored in array pixels
-				frame.ptr(frame.rows()/2, frame.cols()/2).get(bgr);
-				for(int i = 0; i < 3; i++){
-					bgrUnsigned[i] = bgr[i] & 0xFF;
+			Mat[] frameCopies = new Mat[6];
+			Mat[] croppedFrames = new Mat[6];
+			Rect roi = new Rect((CAMERA_WIDTH - DISPLAY_WIDTH) / 2, (CAMERA_HEIGHT - DISPLAY_HEIGHT) / 2, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+			int ind = 0;
+
+			byte[] bgr = new byte[3];
+
+			//collect images
+			for(int i = 0; i < 6; i++){
+				if (capture.read(frame)) {
+					frameCopies[ind++] = frame.clone();
+
+					//get pixels with: frame.ptr(x, y).get(bgr);, bgr will store the pixel data in blue,green,red format
+					frame.ptr(frame.rows()/2, frame.cols()/2).get(bgr);
+					System.out.println(byteArrayToString(bgr));
 				}
-				System.out.println(Arrays.toString(bgrUnsigned));
+	
+				delay(1000);
 			}
+
+			// crop all images
+			for (int i = 0; i < 6; i++) {
+				croppedFrames[i] = new Mat(frameCopies[i], roi);
+			}
+
+			// Place each image in its position
+			for (int i = 0; i < 6; i++) {
+				int x = positions[i][0] * DISPLAY_WIDTH;
+				int y = positions[i][1] * DISPLAY_HEIGHT;
+				Mat insert = displayGrid.apply(new Rect(x, y, DISPLAY_WIDTH, DISPLAY_HEIGHT));
+				croppedFrames[i].copyTo(insert);
+			}
+
+			//Display grid
+			CanvasFrame canvas = new CanvasFrame("Cube Map", 1.0);
+			canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+			canvas.showImage(converter.convert(displayGrid));
+			converter.close();
 		}
 
 
@@ -171,13 +221,14 @@ public class Main {
 		}
 		if(USING_WEBCAM){
 			capture.close();
-			capture.release();
 		}
 		printStatusUpdate("MAIN TERMINATED");
 	}
 
-	public static void printByteArrayAsString(byte[] arr){
-		for(byte b : arr) System.out.print((char)b);
+	public static String byteArrayToString(byte[] arr){
+		String str = "[" + Byte.toUnsignedInt(arr[0]);
+		for(int i = 1; i < arr.length; i++) str += ", " + Byte.toUnsignedInt(arr[i]);
+		return str + "]";
 	}
 
 	private static void initializeSolveData(){
